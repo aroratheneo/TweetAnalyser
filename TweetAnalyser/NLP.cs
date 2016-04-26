c1d55efcb1c1f1c0b5daf7a9ddbae43492b05a7b
Himanshu Arora <aroratheneo@gmail.com>
2016-04-26 22:13:20 +0530

    Added Tweets Changes
    Tweets Analyser Added

---
+++ TweetAnalyser/NLP.cs
@@ -0,0 +1,51 @@
+﻿using edu.stanford.nlp.pipeline;
+using java.io;
+using java.util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TweetAnalyser
+{
+    public static class NLP {
+
+        public static void GetSentiment()
+        {
+            var jarRoot = @"C:\Program Files (x86)\Java\jre1.8.0_66\lib";
+            // Text for processing
+            var text = "Kosgi Santosh sent an email to Stanford University. He didn't get a reply.";
+
+            // Annotation pipeline configuration
+            var props = new Properties();
+            props.put("pos.model", "edu/stanford/nlp/models/pos-tagger/wsj-bidirectional/wsj-0-18-bidirectional-distsim.tagger");
+            // using standard pipeline
+
+            props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+            props.setProperty("ner.useSUTime", "0");
+
+            // We should change current directory, so StanfordCoreNLP could find all the model files automatically
+            var curDir = Environment.CurrentDirectory;
+            Directory.SetCurrentDirectory(jarRoot);
+            var pipeline = new StanfordCoreNLP(props);
+            Directory.SetCurrentDirectory(curDir);
+
+            // Annotation
+            var annotation = new Annotation(text);
+            pipeline.annotate(annotation);
+
+            // Result - Pretty Print
+            using (var stream = new ByteArrayOutputStream())
+            {
+                pipeline.prettyPrint(annotation, new PrintWriter(stream));
+                System.Console.WriteLine(stream.toString());
+                stream.close();
+            }
+        }
+
+    
+}
+}
---
+++ TweetAnalyser/Program.cs
@@ -0,0 +1,94 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace TweetAnalyser
+{
+    class Program
+    {
+        public static string _consumerKey = "Hbr638SHLTfru6mnMFGjWyLLK"; // Add your Key
+        public static string _consumerSecret = "ItOs0SznVDMYbv0gwEXbf3ewI7gM5jtGUAkx4bxAlJRIqN59cZ"; // Add your Key
+        public static string _accessToken = "59375508-rRY3UB7FBBeswoOTXP10pGTm6og5UDVjYIGZyUDNu"; // Add your Key
+        public static string _accessTokenSecret = "yd1U4jd0jeiiJTcNmh00pXZJ8IIFOwttVptUEd3jXbw5y"; // Add your Key
+
+        static bool canBreak = false;
+
+        static void Main(string[] args)
+        {
+            //NLP.GetSentiment();
+            long maxId = GetTweets(null);
+
+            while (!canBreak)
+            {
+             maxId =    GetTweets(maxId);
+            }
+            Console.ReadLine();
+        }
+
+        private static long GetTweets(long? maxId)
+        {
+            TwitterService twitterService = new TwitterService(_consumerKey, _consumerSecret);
+            twitterService.AuthenticateWith(_accessToken, _accessTokenSecret);
+
+            int tweetcount = 1;
+            TwitterSearchResult tweets_search = null;
+            if (maxId.HasValue)
+            {
+                tweets_search = twitterService.Search(new SearchOptions { Q = "@NatWest_Help", MaxId = maxId });
+            }
+            else
+            {
+                tweets_search = twitterService.Search(new SearchOptions { Q = "@NatWest_Help" });
+            }
+            //Resulttype can be TwitterSearchResultType.Popular or TwitterSearchResultType.Mixed or TwitterSearchResultType.Recent
+            
+            List<TwitterStatus> resultList = new List<TwitterStatus>(tweets_search.Statuses);
+            if (resultList.Count == 0)
+            {
+                canBreak = true;
+            }
+            foreach (var tweet in tweets_search.Statuses)
+            {
+                try
+                {
+
+
+                    //Above are the things we can also get using TweetSharp.
+                    Console.WriteLine("TweetId : " + tweet.Id + " Message: " + tweet.Text);
+
+                    InsertMongoDB(tweet);
+                    maxId = tweet.Id;
+                    if (tweet.CreatedDate.CompareTo(DateTime.Now.AddYears(-1)) < 0)
+                    {
+                        canBreak = true;
+                    }
+                    tweetcount++;
+                }
+                catch { }
+            }
+
+            return maxId.Value;
+
+        }
+
+        private static void InsertMongoDB(TwitterStatus tweet)
+        {
+            try
+            {
+                string connectionString = "mongodb://localhost:27017";
+                MongoClient client = new MongoClient(connectionString);
+                var database = client.GetDatabase("TweetsAnalyzer");
+                var tweetsCollection = database.GetCollection<TwitterStatus>("Tweets");
+                tweetsCollection.InsertOne(tweet);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
